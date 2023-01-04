56daaf8a3751da264d6db72fd049287b39d148ef
sedtawut35085 <73195479+sedtawut35085@users.noreply.github.com>
2023-01-04 15:20:36 +0700

    “update”

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Uptodate 1.0.0
+          Uptodate 2.0.0
         </p>
         <a
           className="App-link"
