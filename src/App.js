dc6482dff32af972667360112f00ee572494de8b
wattanasiri <bright.wattana@gmail.com>
2023-02-14 00:45:56 +0700

    create top bar

--- src/App.js
+++ src/App.js
@@ -2,23 +2,40 @@ import "./assets/styles/App.css"
 import "./assets/styles/Background.css"
 import "./assets/styles/Components.css"
 import "./assets/styles/Sign.css"
+import "./assets/styles/Layout.css"
 
-import { BrowserRouter , Route , Routes} from "react-router-dom";
+import { BrowserRouter, Route, Routes} from "react-router-dom";
 
 import SignInForm from "./pages/Sign/signIn.js"
 import SignUpForm from "./pages/Sign/signUp.js"
 import SelectRole from "./pages/Sign/selectRole.js"
 import PendingProf from "./pages/Sign/pendingProf.js"
 
+import AppLayout from "./layout/index.js"
+
+const Home = () => <h2 style={{paddingTop: 100}}>Home</h2>;
+const Discuss = () => <h2 style={{paddingTop: 100}}>Discuss</h2>;
+const Raking = () => <h2 style={{paddingTop: 100}}>Raking</h2>;
+const Store = () => <h2 style={{paddingTop: 100}}>Store</h2>;
+const Weekly = () => <h2 style={{paddingTop: 100}}>Weekly</h2>;
+
+
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<SignInForm />}></Route>
-          <Route path="/sign-up" element={<SignUpForm />}></Route>
-          <Route path="/select-role" element={<SelectRole />}></Route>
-          <Route path="/pending-prof" element={<PendingProf />}></Route>
+          <Route exact path="/" element={<SignInForm />} />
+          <Route exact path="/sign-up" element={<SignUpForm />} />
+          <Route exact path="/select-role" element={<SelectRole />} />
+          <Route exact path="/pending-prof" element={<PendingProf />} />
+          <Route element={<AppLayout />}>
+            <Route path="/home" element={<Home />}/>
+            <Route path="/discuss" element={<Discuss/>}/>
+            <Route path="/raking" element={<Raking />}/>
+            <Route path="/store" element={<Store />}/>
+            <Route path="/weekly" element={<Weekly />}/>
+          </Route>
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/layout/topbar.js
@@ -0,0 +1,109 @@
+import React, { useState, useEffect, useRef } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+
+import { HiOutlineCalendar, HiOutlineBell } from 'react-icons/hi'
+import { FiChevronRight } from 'react-icons/fi'
+import { IoPersonCircleOutline, IoPersonOutline, IoGiftOutline, IoSettingsOutline, IoExitOutline } from 'react-icons/io5'
+
+const TopBar = () => {
+
+    const pathname = useLocation().pathname
+    const [dropdownActive, setDropdownActive] = useState(false)
+
+    const refOne = useRef(null)
+
+    const handleClickOutside = (e) => {
+        if(!refOne.current.contains(e.target)) {
+            setDropdownActive(false)
+        }
+    }
+
+    useEffect(() => {
+        document.addEventListener("click", handleClickOutside, true)
+
+        return () => {
+            document.removeEventListener('click', handleClickOutside, true);
+        }
+    }, [])
+
+    return (
+        <div className="topbar">
+            <nav>
+                <Link className={`nav hover ${pathname === "/home" ? "active" : ""}`} to="/home">
+                    <div>
+                        <img alt="logo-text(bold).png" src="/assets/images/logo/logo-text(bold).png" height="32"/>
+                    </div>
+                </Link>
+                <Link className={`nav hover ${pathname === "/discuss" ? "active" : ""}`} to="/discuss">
+                    <div>Discuss</div>
+                </Link>
+                <Link className={`nav hover ${pathname === "/raking" ? "active" : ""}`} to="/raking">
+                    <div>Raking</div>
+                </Link>
+                <Link className={`nav hover ${pathname === "/store" ? "active" : ""}`} to="/store">
+                    <div>Store</div>
+                </Link>
+                <Link className={`nav ${pathname === "/weekly" ? "active" : ""}`} to="/weekly">
+                    <div>
+                        <HiOutlineCalendar size={24} />
+                        <span className="badge">.</span>
+                    </div>
+                </Link>
+            </nav>
+            <div className="profile-section">
+                <div className="nav">
+                    <div className="notification">
+                        <div className="icon">
+                            <HiOutlineBell size={24} />
+                            <span className="badge">.</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="nav">
+                    <div className="point">999 P</div>
+                </div>
+                <div className="nav">
+                    <div className="profile-pic">
+                        <div className="img" onClick={() => setDropdownActive(!dropdownActive)}>
+                            <IoPersonCircleOutline />
+                        </div>
+                        <div className={`dropdown ${dropdownActive ? "active" : ""}`} ref={refOne}>
+                            <div className="info">
+                                <span className="techup-id f-md">Techup_ID</span>
+                                <span className="full-name f-sm color-gray2">Name Surname</span>
+                            </div>
+                            <div className="px-2">
+                                <div className="divider my-3"></div>
+                            </div>
+                            <div className="menu">
+                                <Link className="d-flex jc-btw">
+                                    <div>
+                                        <IoPersonOutline size={32} className="me-3" />Profile
+                                    </div> 
+                                    <FiChevronRight size={28} />
+                                </Link>
+                                <Link className="d-flex jc-btw">
+                                    <div>
+                                        <IoGiftOutline size={32} className="me-3" />Point
+                                    </div>
+                                    <FiChevronRight size={28} />
+                                </Link>
+                                <Link className="d-flex jc-btw">
+                                    <div>
+                                        <IoSettingsOutline size={32} className="me-3" />Settings
+                                    </div>
+                                    <FiChevronRight size={28} />
+                                </Link>
+                                <Link>
+                                    <IoExitOutline size={32} className="me-3" />Sign out
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default TopBar;
