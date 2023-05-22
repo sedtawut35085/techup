e2499dc1408d42844d1b17506dbe86188cd27d52
wattanasiri <bright.wattana@gmail.com>
2023-05-22 17:17:26 +0700

    edit point history page

--- src/pages/Profile/pointHistory.js
+++ src/pages/Profile/pointHistory.js
@@ -11,6 +11,29 @@ function PointHistory() {
                 <div className="body">
                     <div className="redeem-reward">
                         <span className="title"><TbGift className="color-1" />Redeem reward</span>
+                        <div className="item-wrap">
+                            <div className="redeem-card">
+                                <div className="detail-hover">
+                                    <span>T-Shirt</span>
+                                    <button>See details</button>
+                                </div>
+                                <img alt="T-Shirt" src="https://sp-ao.shortpixel.ai/client/to_webp,q_glossy,ret_img,w_510/https://www.moment-tshirt.com/wp-content/uploads/2021/02/Soft-Premium-Orange-scaled-510x510.jpg"/>
+                            </div>
+                            <div className="redeem-card">
+                                <div className="detail-hover">
+                                    <span>Pen</span>
+                                    <button>See details</button>
+                                </div>
+                                <img alt="Pen" src="https://www.premiums.co.th/wp-content/uploads/2021/11/PP9289-FSCI-KMUTT2.jpg" />
+                            </div>
+                            <div className="redeem-card">
+                                <div className="detail-hover">
+                                    <span>Bubble Tea</span>
+                                    <button>See details</button>
+                                </div>
+                                <img alt="Bubble Tea" src="https://i.pinimg.com/736x/1e/42/c6/1e42c6c9e4c234e2f843426675a26d48.jpg"/>
+                            </div>
+                        </div>
                     </div>
                     <div className="log-point">
                         <span className="title"><TbHistory className="color-1" />Points history</span>
